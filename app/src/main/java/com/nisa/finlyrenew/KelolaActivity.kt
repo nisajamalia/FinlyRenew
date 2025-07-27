c3ec05f7d4838bc099cf66cb3be5a1da00efead5
Nisa Jamalia Nurhayati <nisajamalia99@gmail.com>
2025-07-27 19:28:41 +0700

    updatekelola

--- app/src/main/java/com/nisa/finlyrenew/KelolaActivity.kt
+++ app/src/main/java/com/nisa/finlyrenew/KelolaActivity.kt
@@ -1,6 +1,10 @@
 package com.nisa.finlyrenew
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +14,21 @@ class KelolaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        supportActionBar?.hide()
         setContentView(R.layout.activity_kelola)
+
+        val btnback = findViewById<ImageView>(R.id.btn_back)
+
+        btnback.setOnClickListener {
+            finish()
+
+        }
+        val btnTambah = findViewById<Button>(R.id.btn_tambah_transaksi)
+
+        btnTambah.setOnClickListener {
+            val intent = Intent(this, KelolaDetailActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
